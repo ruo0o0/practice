15bced2942d3a6a4574b55c8817a415f65a4b383
ruo0o0 <ruon0o0@gmail.com>
2021-12-25 20:21:29 +0900

    プロフィール画面(仮)を作成

---
+++ comparative-music/src/views/Profile.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="container">
+    <v-card width="300">
+      <v-list-item>
+        <v-list-item-content class="py-2">
+          <v-list-item-title class="text-center mb-2">hoge</v-list-item-title>
+          <v-list-item-title class="mb-2">
+            <v-img width="128" :src="photoURL" aspect-ratio="1" class="mx-auto"></v-img>
+          </v-list-item-title>
+          <!-- <v-btn class="mx-auto">フォローする</v-btn> -->
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list dense class="py-0">
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>鑑賞データ</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>演奏者</v-list-item-title>
+          </v-list-item-content>
+          <p class="text-body-2 my-2" >{{ artists }}人</p>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>曲</v-list-item-title>
+          </v-list-item-content>
+          <p class="text-body-2 my-2" >{{ album.length }}曲</p>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>感想</v-list-item-title>
+          </v-list-item-content>
+          <p class="text-body-2 my-2" >{{ comments }}曲</p>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>プロフィール</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
+        <!-- <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>フォロー</v-list-item-title>
+          </v-list-item-content>
+          <p class="text-body-2 my-2" >3人</p>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>フォロワー</v-list-item-title>
+          </v-list-item-content>
+          <p class="text-body-2 my-2" >3人</p>
+        </v-list-item> -->
+        <v-divider></v-divider>
+        <v-list-item>
+          <v-list-item-title>自己紹介</v-list-item-title>
+        </v-list-item>
+        <p class="font-size px-4">Write something you want to appeal</p>
+      </v-list>
+    </v-card>
+    <v-card width="600" class="ml-4" min-height="100">
+      <v-card-title class="subtitle-1 py-2">hogeさんの感想</v-card-title>
+      <v-divider></v-divider>
+      <div
+        v-for="(music, index) in filteredAlbum"
+        :key="index"
+      >
+        <div class="flex mt-2 ml-2">
+          <div>
+            <v-avatar tile rounded="sm">
+              <v-img :src="photoURL" aspect-ratio="1"></v-img>
+            </v-avatar>
+          </div>
+          <div class="flex-grow">
+            <p class="ml-2 mb-2">hoge</p>
+            <v-card color="grey darken-4" class="ma-2">
+              <v-card-title class="subtitle-1">{{ music.title }}</v-card-title>
+              <v-card-subtitle class="py-0">{{ music.artist }}</v-card-subtitle>
+              <v-card-text class="pb-0">
+                <v-textarea
+                  background-color="#1E1E1E"
+                  :value="music.comment"
+                  readonly
+                  loading="false"
+                >
+                </v-textarea>
+              </v-card-text>
+            </v-card>
+          </div>
+        </div>
+        <v-divider></v-divider>
+      </div>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  data () {
+    return {
+      album: [],
+    }
+  },
+  created () {
+    this.album = this.$store.state.album
+  },
+  computed: {
+    artists: function () {
+      return (this.album.filter((music, index, self) => self.findIndex(e => e.artist === music.artist) === index)).length
+    },
+    comments: function () {
+      return (this.album.filter(music => music.comment)).length
+    },
+    filteredAlbum: function () {
+      return this.album.filter(music => music.comment)
+    },
+    ...mapGetters(['photoURL'])
+  }
+}
+</script>
+
+<style scoped>
+  .container {
+    display: flex;
+    justify-content: center;
+    margin-top: 40px;
+    align-items: flex-start;
+  }
+  .flex {
+    display: flex;
+  }
+  .flex-grow {
+    flex-grow: 1;
+  }
+  .font-size {
+    font-size: 0.8125rem;
+  }
+</style>
