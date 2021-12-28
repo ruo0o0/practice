d8a2c81f73d0205345814ae37dff1f9a8308192d
ruo0o0 <ruon0o0@gmail.com>
2021-12-28 21:04:29 +0900

    ナビゲーション表示がルート対応するよう修正

--- comparative-music/src/components/SideNav.vue
+++ comparative-music/src/components/SideNav.vue
@@ -2,7 +2,7 @@
   <v-navigation-drawer v-model="$store.state.drawer" fixed temporary right>
     <v-list nav>
       <v-list-item-group>
-        <v-list-item v-for="(menuItem, index) in menuItems" :key="index" @click="$router.push(menuItem.url)">
+        <v-list-item v-for="(menuItem, index) in menuItems" :key="index" :to="menuItem.url">
           <v-list-item-title>{{ menuItem.name }}</v-list-item-title>
         </v-list-item>
       </v-list-item-group>
